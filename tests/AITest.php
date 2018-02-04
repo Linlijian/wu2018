ddaf329a515ff5c6a72fe47e8407417d6d0219c4
linlijian <praw__rul2__lvluk@outlook.co.th>
2018-02-04 04:08:51 +0000

    test add

--- tests/AITest.php
+++ tests/AITest.php
@@ -10,4 +10,16 @@ final class AITest extends TestCase
         $expected_result = 'Male';
         $this->assertEquals($expected_result, $result);
     }
+    public function testGender_Female(): void
+    {
+        $result = AI::getGender('สวัสดีค่ะ');
+        $expected_result = 'Female';
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGender_Positive(): void
+    {
+        $result = AI::getGender('น่ารัก');
+        $expected_result = 'Positive';
+        $this->assertEquals($expected_result, $result);
+    }
 }
